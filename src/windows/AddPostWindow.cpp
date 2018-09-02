25609401e3e846feaca30fe6ebe0f57b2a2c7360
Harystolho <haristolhogamer@gmail.com>
2018-09-01 21:25:21 -0300

    added buttons for add post window

--- src/windows/AddPostWindow.cpp
+++ src/windows/AddPostWindow.cpp
@@ -12,15 +12,14 @@ enum ADD_POST_ID {
 };
 
 AddPostWindow::AddPostWindow(wxWindow* parent) :
-		wxFrame(parent, wxID_ANY, "Add Post", wxPoint(0, 0), wxSize(500, 280)) {
+		wxFrame(NULL, wxID_ANY, "Add Post", wxPoint(0, 0), wxSize(500, 280)) {
 	Center();
 
 	pageFrame = static_cast<PageFrame*>(parent);
 
-	createObjects();
-
 	panel = new wxPanel(this, wxID_ANY);
 
+	createObjects();
 }
 
 AddPostWindow::~AddPostWindow() {
@@ -30,18 +29,37 @@ AddPostWindow::~AddPostWindow() {
 void AddPostWindow::createObjects() {
 
 	wxStaticText *titleLabel = new wxStaticText(panel, wxID_ANY, "Title",
-			wxPoint(), wxSize());
+			wxPoint(5, 5));
 	wxStaticText *linkLabel = new wxStaticText(panel, wxID_ANY, "Link",
-			wxPoint(), wxSize());
+			wxPoint(5,
+					titleLabel->GetPosition().y
+							+ titleLabel->GetSize().GetHeight() + 10));
 	wxStaticText *textLabel = new wxStaticText(panel, wxID_ANY, "Text",
-			wxPoint(), wxSize());
+			wxPoint(5,
+					linkLabel->GetPosition().y
+							+ linkLabel->GetSize().GetHeight() + 10));
 
 	title = new wxTextCtrl(panel, TITLE_ID, wxT(""),
-			wxPoint(0, 0));
-	link = new wxTextCtrl(panel, TITLE_ID, wxT(""), wxPoint(0, 0));
-	text = new wxTextCtrl(panel, TEXT_ID, wxT(""), wxPoint(0, 0));
+			wxPoint(
+					titleLabel->GetPosition().x
+							+ titleLabel->GetSize().GetWidth() + 15,
+					titleLabel->GetPosition().y));
+	link = new wxTextCtrl(panel, TITLE_ID, wxT(""),
+			wxPoint(
+					linkLabel->GetPosition().x + linkLabel->GetSize().GetWidth()
+							+ 15, linkLabel->GetPosition().y));
+	text = new wxTextCtrl(panel, TEXT_ID, wxT(""),
+			wxPoint(
+					textLabel->GetPosition().x + textLabel->GetSize().GetWidth()
+							+ 15, textLabel->GetPosition().y));
 	date = new wxDatePickerCtrl(panel, DATE_ID, wxDateTime::Now(),
-			wxPoint(0, 0));
-	add = new wxButton(panel, ADD_ID, wxT("Add Post"));
+			wxPoint(5,
+					textLabel->GetPosition().y
+							+ textLabel->GetSize().GetHeight() + 10));
+	add = new wxButton(panel, ADD_ID, wxT("Add Post"),
+			wxPoint(5,
+					date->GetPosition().y + date->GetSize().GetHeight() + 10));
+
+
 
 }
